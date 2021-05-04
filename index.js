62a9ba91d6cc2ce5c3eaf8725566a9d704e5dbc3
mabenj <markobenj@hotmail.com>
2021-05-04 20:59:33 +0300

    Add minimist

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
 const fs = require("fs");
 const path = require("path");
 
-const [, , powerMode, configPath] = process.argv;
+var argv = require("minimist")(process.argv.slice(2));
 
 let config = null;
 try {
-	var dirname = configPath ? path.dirname(configPath) : process.cwd();
+	var dirname = argv.config ? path.dirname(argv.config) : process.cwd();
 	config = JSON.parse(fs.readFileSync(path.join(dirname, "./config.json")));
 } catch {
-	console.log("Could not open the 'config.json' file: ");
+	console.log("Could not open the 'config.json' file");
 }
 
-if (powerMode && config) {
-	const rigAction = require("./src/rigaction");
-	rigAction.setPowerMode(config, powerMode.toUpperCase());
-} else if (!powerMode) {
-	console.log("Give a powermode as an argument (LOW, MEDIUM or HIGH)");
-}
-
-(async () => {
-	await sleep(5000);
-})();
-
-function sleep(millis) {
-	return new Promise((resolve) => setTimeout(resolve, millis));
+if (argv.mode && config) {
+	const { setPowerMode } = require("./src/SetPowerMode");
+	setPowerMode(config, argv.mode.toUpperCase());
+} else if (!argv.mode) {
+	console.log("Specify a power mode with '--mode' (LOW, MEDIUM or HIGH)");
 }
